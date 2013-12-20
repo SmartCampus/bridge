097dd37a45e23ce1852abe9800e24146e47f2905
Sébastien Mosser <sebastien.mosser@gmail.com>
2013-12-20 10:27:26 +0100

    initial skeleton

---
+++ bridge/src/main/java/fr/unice/smart_campus/App.java
@@ -0,0 +1,13 @@
+package fr.unice.smart_campus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
