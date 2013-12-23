04dd70fd1efe2593634bf3075a5554f4e339122f
Jean <jean.oudot.06@gmail.com>
2013-12-23 13:43:01 +0100

    Code update
    Add main and main configuration

---
+++ bridge/src/main/java/fr/unice/smart_campus/Main.java
@@ -0,0 +1,13 @@
+
+package fr.unice.smart_campus;
+
+/**
+ * Program main.
+ * 
+ * @author  Jean Oudot - IUT Nice / Sophia Antipolis - S4D
+ * @version 1.0.0
+ */
+public class Main
+{
+
+}
---
+++ bridge/src/main/java/fr/unice/smart_campus/MainConfig.java
@@ -0,0 +1,13 @@
+
+package fr.unice.smart_campus;
+
+/**
+ * Create the configuration of the all program.
+ * 
+ * @author  Jean Oudot - IUT Nice / Sophia Antipolis - S4D 
+ * @version 1.0.0
+ */
+public class MainConfig
+{
+
+}
